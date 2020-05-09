08e318cf5c706a0831c432caa151a85dbbcc98d5
unknown <farmanupdate@gmail.com>
2020-05-09 20:59:45 +0530

    Adding to the branch

--- src/test/java/restassured/restAssuredclass.java
+++ src/test/java/restassured/restAssuredclass.java
@@ -36,36 +36,7 @@ public class restAssuredclass {
 	  RestAssured.basePath = "/v2";
 	  
   }
-//  
-//  @DataProvider
-//  public Object[][] getDatausingDataProvider() {
-//		 
-//		 Object[][] TestData = new Object[][] { 
-//			
-//			 				   new Object[] {4, 5, "TestCategory","TestString", "www.google.com","www.ust.com","www.pst.com",11,"testing",12,"resting","available" },
-//			 				  new Object[] {14, 15, "TestCategory","TestString", "www.google.com","www.ust.com","www.pst.com",14,"testing",15,"resting","available" }};
-//		 return TestData;
-//	 }
-//		 
-//	@Test(dataProvider = "getDatausingDataProvider")
-//	public void createPetwithDataProvider(int petId, int cId, String cName, String Name, String p1, String p2, String p3, int tId, String tName, int t1_Id, String t1_Name, String Status ) {
-//		
-//		Category c = new Category(cId,cName);
-//		Tags t = new Tags(tId,tName);
-//		Tags t1 = new Tags(t1_Id, t1_Name);
-//		String[] ph = new String[] {p1,p2,p3}; //Tag is giving an error ask aman how to resolve
-//		Tags[] tg = new Tags[] {t,t1};
-//		Pet pt = new Pet(petId,c,Name,ph,tg,Status);
-//		
-//		request = RestAssured.given().log().all().contentType(ContentType.JSON).body(pt);
-//		request.log().all();
-//		
-//		Response res = request.post("/pet");
-//		res.then().log().all();
-//		
-//		
-//	}
-//	
+
   @Test(priority=1)
   public void CreatePet() {
 	  
@@ -376,19 +347,6 @@ public class restAssuredclass {
 ////				Response res = request.post("/pet");
 ////				res.then().log().all();
 
-//  
-//  	@Test
-//  	public void uploadTest() {
-//  		
-//  		String Endpoint = "/pet/52/uploadImage";
-//  		String rootPath = System.getProperty("user.dir");
-//  		
-//  		File fileName = new File(rootPath + "\\src\\test\\resources\\TestData\\Payment.json");
-//  		
-//  		Response res = RestAssured.given().log().all().formParam("additionalMetadata", "Testing for upload").multiPart(fileName).post(Endpoint);
-//  		
-//  		res.then().log().all();
-//  	}
-//  	
+
   
 }
